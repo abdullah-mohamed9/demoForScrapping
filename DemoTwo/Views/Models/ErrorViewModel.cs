53cf38817cddf30453f44191f7935e25a79852e7
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-05-06 20:43:08 +0300

    first commit

---
+++ DemoTwo/Views/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoTwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
